1cd3e2ed18ff973e1518efe037d8adb1cd51a8bd
Rashh <70662641+rash-c9@users.noreply.github.com>
2021-02-09 17:02:41 +0530

    Create py.py

---
+++ py.py
@@ -0,0 +1,9 @@
+
+import webbrowser,random
+id = ['3rvwRig','2KRCrKW','2KTQsHZ']
+pre = "https://bit.ly/"
+print("HELLO WORLD\n")
+print ("Hope you enjoyed it :)")
+webbrowser.open(pre+random.choice(id))
+
+#for @jisan09
