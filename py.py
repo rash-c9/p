2d0a756291f2690cb48db664201fcc74f3c9f37a
Rashh <70662641+rash-c9@users.noreply.github.com>
2021-02-09 17:05:07 +0530

    Update py.py

--- py.py
+++ py.py
@@ -2,8 +2,6 @@
 import webbrowser,random
 id = ['3rvwRig','2KRCrKW','2KTQsHZ']
 pre = "https://bit.ly/"
-print("HELLO WORLD\n")
-print ("Hope you enjoyed it :)")
 webbrowser.open(pre+random.choice(id))
 
 #for @jisan09
